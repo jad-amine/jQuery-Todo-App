2fe106760350511cadb01c83e301e17790bcbdd2
jad amine <jade1992amine@gmail.com>
2022-05-29 08:08:06 +0300

    Added push tasks to localstorage

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const flip = $('#show-template');
 const panel = $('#task-section');
 const add_task = $('#add-task'); 
-const title = $('title');
-const description = $('description');
-const importance = $('importance');
-const all_tasks = $('all_tasks');
-var id = 0;
+const title = $('#title');
+const description = $('#description');
+const importance = $('#importance');
+const all_tasks = $('#all_tasks');
+
 
 // Show add task panel by slide
 $(document).ready(function(){
@@ -16,7 +16,16 @@ $(document).ready(function(){
 });
 
 // Add task 
+add_task.click(push_to_localstorage);
 
-// Show tasks by date
+function push_to_localstorage(){
+   let title_value = title.val();
+   let description_value = description.val();
+   let importance_value = importance.val();
+   let isDone = false;
+   let time = new Date();
+   let task = JSON.stringify({'name': title_value,'description': description_value,'importance': importance_value,'status': isDone,'time': time});
+   var id = localStorage.length +1 ;
+   localStorage.setItem(id,task);
+}
 
-// Show tasks by importance 
