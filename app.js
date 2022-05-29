789cb3a12a5faec0149075ff32bac2412a8e082a
jad amine <jade1992amine@gmail.com>
2022-05-29 06:50:48 +0300

    JS - Grabed elements and declared variables

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+// Declare variable and grab elements
 const flip = $('#show-template');
 const panel = $('#task-section');
+const add_task = $('#add-task'); 
+const title = $('title');
+const description = $('description');
+const importance = $('importance');
+const all_tasks = $('all_tasks');
+var id = 0;
 
+// Show add task panel by slide
 $(document).ready(function(){
    flip.click(function(){
    panel.slideToggle("slow");
    });
 });
+
+// Add task 
+
+// Show tasks by date
+
+// Show tasks by importance 
